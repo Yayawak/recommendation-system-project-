3cfe706dfa13fe71f08cf4fe018dda59ac84911c
jaikwang <kanna9764@gmail.com>
2024-09-06 12:19:35 +0700

    setup react

---
+++ front/recommendation-app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Home.css'; 
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>เบื่อนิม</h1>
+    </div>
+  );
+};
+
+export default Home;
