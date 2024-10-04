52b15b19ac14ad297964f331d23a4358b6d574d9
Iampipos <nakbutsaraphon@gmail.com>
2024-10-04 10:31:11 +0700

    add URL photo Home.js

--- front/recommendation-app/src/pages/Home.js
+++ front/recommendation-app/src/pages/Home.js
@@ -5,12 +5,12 @@ import SearchResultsList from '../components/SearchResultsList';
 import ButtonGroup from '../components/ButtonGroup';
 
 const fashionItems = [
-  { id: 1, src: "https://example.com/dress1.jpg", alt: "Pink dress" },
-  { id: 2, src: "https://example.com/pants1.jpg", alt: "Black pants" },
-  { id: 3, src: "https://example.com/dress2.jpg", alt: "Blue floral dress" },
-  { id: 4, src: "https://example.com/dress3.jpg", alt: "Red dress" },
-  { id: 5, src: "https://example.com/dress4.jpg", alt: "Green dress" },
-  { id: 6, src: "https://example.com/pants2.jpg", alt: "Blue jeans" },
+  { id: 1, src: "https://i.pinimg.com/564x/12/7f/0d/127f0d2ba8c221f9969084b1a95c7079.jpg", alt: "Pink dress" },
+  { id: 2, src: "https://i.pinimg.com/474x/a7/cd/21/a7cd21ef9f6690022b99192eac700a90.jpg", alt: "Black pants" },
+  { id: 3, src: "https://i.pinimg.com/474x/49/7f/55/497f554448e8a45b4055f4bbac6f9a76.jpg", alt: "Blue floral dress" },
+  { id: 4, src: "https://i.pinimg.com/474x/bb/04/67/bb0467b77d3524eb6c3a7f878fbed598.jpg", alt: "Red dress" },
+  { id: 5, src: "https://i.pinimg.com/474x/44/56/88/445688524356a43305a585b0c17c67f5.jpg", alt: "Green dress" },
+  { id: 6, src: "https://i.pinimg.com/474x/5f/47/a7/5f47a736d11efc985e48252bd8e0a369.jpg", alt: "Blue jeans" },
 ];
 
 
