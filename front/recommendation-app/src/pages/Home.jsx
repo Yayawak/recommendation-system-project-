e62e819125ee5744bf73907dbdcee1e53b0a83f2
bell <ppasikapong@gmail.com>
2024-10-06 00:35:21 +0700

    delete searchResultList in homepage

--- front/recommendation-app/src/pages/Home.jsx
+++ front/recommendation-app/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import '../assets/styles/Home.css';
 import SearchBar from '../components/SearchBar';
-import SearchResultsList from '../components/SearchResultsList';
 import ButtonGroup from '../components/ButtonGroup';
 import { Link } from 'react-router-dom';
 
@@ -15,7 +14,7 @@ const fashionItems = [
 ];
 
 const Home = () => {
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("");
 
   const filteredItems = fashionItems.filter((item) =>
     item.alt.toLowerCase().includes(searchTerm.toLowerCase())
@@ -25,9 +24,6 @@ const Home = () => {
     <div className="home-page">
       <div className='search-bar-container'>
         <SearchBar setSearchTerm={setSearchTerm} />
-        <div className="search-results-container">
-          <SearchResultsList results={filteredItems} />
-        </div>
         <div className="button-group-container">
           <ButtonGroup />
         </div>
